bcf9f96b7fff4c79a1d426bd8c3e613b5efd42f6
sabo110 <samirphanuel@gmail.com>
2024-02-17 09:34:45 +0100

    ajout des cdn de flowbite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app:{
+    head:{
+     link:[
+      {rel:'stylesheet', href:'https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css'}
+     ],
+     script:[
+      {src:'https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js', defer:true}
+     ]
+    },
+    
+  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
